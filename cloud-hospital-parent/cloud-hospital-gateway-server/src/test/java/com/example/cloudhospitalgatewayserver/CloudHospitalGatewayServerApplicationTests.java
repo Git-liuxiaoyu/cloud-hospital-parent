37eb4bed45524214f3113415b1fb906b4d6c99ce
Alnwick11AtoZ <9042793+alnwick11atoz@user.noreply.gitee.com>
2021-06-18 18:13:40 +0800

    !25 完成 - 后端 - 新建 - 项目网关（Gateway） - 张方松 - #I3W5RN * - 添加了跨域过滤 * - 增加了 Gateway 跨域请求配置 * 后端 - 新建 - 项目网关（Gateway） - 张方松 - v2 * 后端 - 新建 - 项目网关（Gateway） - 张方松 - v1

---
+++ cloud-hospital-parent/cloud-hospital-gateway-server/src/test/java/com/example/cloudhospitalgatewayserver/CloudHospitalGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudhospitalgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudHospitalGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
